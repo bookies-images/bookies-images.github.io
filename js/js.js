49b79bd583f486d0b5a1c79be5ad37cf246582ff
bookies-images <atanas.ivanov@efbet.com>
2020-01-27 17:38:16 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-  var fileExt = [".png", ".jpg", ".PNG", ".JPG"];
-  $.ajax({
-    url: 'img/26.01/',
-    success: function(data) {
-      $(data).find("a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + "),a:contains(" + fileExt[3] + ")").each(function() {
-        // var filename = this.href.replace(window.location.host, "").replace("http://", "http://127.0.0.1:5500");
-        // var filename = this.href.replace(window.location.host, "").replace("http://", "https://bookies-images.github.io");
-        var filename = $(this).attr("href");
-        console.log(filename);
+  // var fileExt = [".png", ".jpg", ".PNG", ".JPG"];
+  // $.ajax({
+  //   url: 'img/26.01/',
+  //   success: function(data) {
+  //     $(data).find("a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + "),a:contains(" + fileExt[3] + ")").each(function() {
+  //       // var filename = this.href.replace(window.location.host, "").replace("http://", "http://127.0.0.1:5500");
+  //       // var filename = this.href.replace(window.location.host, "").replace("http://", "https://bookies-images.github.io");
+  //       var filename = $(this).attr("href");
+  //       console.log(filename);
 
-        // $("#fileNames").append("<li>" + '<a href="' + filename + '">' + filename + '</a>' + "</li>");
+  //       // $("#fileNames").append("<li>" + '<a href="' + filename + '">' + filename + '</a>' + "</li>");
+  //     });
+  //   }
+  // });
+
+
+
+  var folder = "img/26.01//";
+
+  $.ajax({
+    url: folder,
+    success: function(data) {
+      $(data).find("a").attr("href", function(i, val) {
+        if (val.match(/\.(jpe?g|png|gif)$/)) {
+          // $("body").append("<img src='" + folder + val + "'>");
+          console.log(folder + val);
+        }
       });
     }
   });
