16765660bb8a5e357ae6fd12901422c3a25fa5de
bookies-images <atanas.ivanov@efbet.com>
2020-01-27 17:39:36 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -23,8 +23,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
     success: function(data) {
       $(data).find("a").attr("href", function(i, val) {
         if (val.match(/\.(jpe?g|png|gif)$/)) {
-          // $("body").append("<img src='" + folder + val + "'>");
-          console.log(folder + val);
+          $("body").append("<img src='" + folder + val + "'>");
+          // console.log(folder + val);
         }
       });
     }
