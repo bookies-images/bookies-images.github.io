cd1cb84fad6c0f389587ab802e22d4b188e9d530
bookies-images <atanas.ivanov@efbet.com>
2020-01-27 17:17:08 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   var fileExt = [".png", ".jpg", ".PNG", ".JPG"];
   $.ajax({
-    type: "GET",
     url: '../img/26.01/',
     success: function(data) {
       $(data).find("a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + "),a:contains(" + fileExt[3] + ")").each(function() {
         // var filename = this.href.replace(window.location.host, "").replace("http://", "http://127.0.0.1:5500");
         // var filename = this.href.replace(window.location.host, "").replace("http://", "https://bookies-images.github.io");
         var filename = $(this).attr("href");
-        console.log("https://bookies-images.github.io" + filename);
+        console.log(filename);
 
         // $("#fileNames").append("<li>" + '<a href="' + filename + '">' + filename + '</a>' + "</li>");
       });
